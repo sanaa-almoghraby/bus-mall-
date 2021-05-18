6b2bd3d066fab87f9cf638a16fd717f09b6737d5
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-05-18 22:12:17 +0300

    lab13

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ let secondimag;
 let thirdimag;
 let voting = 25;
 let userClic = 0;
-let productName=[];
-let countVote=[];
-let productShown=[];
+let productName = [];
+let countVote = [];
+let productShown = [];
 
 function CAtalogbusmall(name, path, times) {
     this.name = name;
@@ -50,45 +50,55 @@ function gitRandomImage() {
     return Math.floor(Math.random() * CAtalogbusmall.busmall.length);
 }
 // console.log(Math.floor(Math.random() * busmall.length));
-//------------------------------ Add local storage-----------------------------------
-function setProduct() {
-    let productdata = JSON.stringify(CAtalogbusmall.busmall);
-    console.log(productdata);
-    localStorage.setItem('CAtalogbusmall', productdata);
-}
+
 
 function randomImage() {
 
     firstimag = gitRandomImage();
     secondimag = gitRandomImage();
     thirdimag = gitRandomImage();
-    
-    let checkimg = [CAtalogbusmall.busmall[firstimag].name ,CAtalogbusmall.busmall[secondimag].name,CAtalogbusmall.busmall[thirdimag].name];
+
+    let checkimg = [CAtalogbusmall.busmall[firstimag].name, CAtalogbusmall.busmall[secondimag].name, CAtalogbusmall.busmall[thirdimag].name];
     console.log(checkimg);
-    
+
     do {
         secondimag = gitRandomImage();
         thirdimag = gitRandomImage();
-    } while (secondimag === thirdimag || secondimag === firstimag || firstimag === thirdimag || firstimag === checkimg[0] || secondimag===checkimg[1] || thirdimag===checkimg[2]);
-    
+    } while (secondimag === thirdimag || secondimag === firstimag || firstimag === thirdimag || firstimag === checkimg[0] || secondimag === checkimg[1] || thirdimag === checkimg[2]);
+
     // console.log(busmall[firstimag]);
     // console.log(busmall[secondimag]);
     // console.log(busmall[thirdimag]);
     firstimgElement.src = CAtalogbusmall.busmall[firstimag].path
-    secondimgElement.src =CAtalogbusmall. busmall[secondimag].path
+    secondimgElement.src = CAtalogbusmall.busmall[secondimag].path
     thirdimgElement.src = CAtalogbusmall.busmall[thirdimag].path
-    
+
     firstimgElement.times = CAtalogbusmall.busmall[firstimag].times++;
     secondimgElement.times = CAtalogbusmall.busmall[secondimag].times++;
     thirdimgElement.times = CAtalogbusmall.busmall[thirdimag].times++;
-    
-    
+
+
 }
 randomImage();
+let displayimg = document.getElementById('busmallimg');
+displayimg.addEventListener('click', clickImage);
 
-firstimgElement.addEventListener('click', clickImage);
-secondimgElement.addEventListener('click', clickImage);
-thirdimgElement.addEventListener('click', clickImage);
+
+// firstimgElement.addEventListener('click', clickImage);
+// secondimgElement.addEventListener('click', clickImage);
+// thirdimgElement.addEventListener('click', clickImage);
+//------------------------------------------------------------------------------------------------
+let formimg = document.getElementById('numproduct');
+formimg.addEventListener('submit', numberOfproduct);
+
+let textOfp = document.getElementById('textofp');
+
+function numberOfproduct(event) {
+    event.preventDefault();
+    voting = event.target.numchoice.value;
+    textOfp.textContent = `The Number of chise broduct is ${voting}`;
+
+}
 
 function clickImage(event) {
     userClic++;
@@ -98,7 +108,7 @@ function clickImage(event) {
         if (event.target.id === 'firstimg') {
             CAtalogbusmall.busmall[firstimag].counterclic = CAtalogbusmall.busmall[firstimag].counterclic + 1;
         } else if (event.target.id === 'secondimg') {
-            CAtalogbusmall.busmall[secondimag].counterclic =CAtalogbusmall.busmall[secondimag].counterclic + 1;
+            CAtalogbusmall.busmall[secondimag].counterclic = CAtalogbusmall.busmall[secondimag].counterclic + 1;
 
         } else {
             CAtalogbusmall.busmall[thirdimag].counterclic = CAtalogbusmall.busmall[thirdimag].counterclic + 1;
@@ -108,9 +118,9 @@ function clickImage(event) {
 
 
     } else {
-        firstimgElement.removeEventListener('click', clickImage);
-        secondimgElement.removeEventListener('click', clickImage);
-        thirdimgElement.removeEventListener('click', clickImage);
+        displayimg.removeEventListener('click', clickImage);
+        // secondimgElement.removeEventListener('click', clickImage);
+        // thirdimgElement.removeEventListener('click', clickImage);
 
 
 
@@ -119,31 +129,33 @@ function clickImage(event) {
 
     randomImage();
 }
-// let liImge = document.getElementById('listresult');
+let liImge = document.getElementById('listresult');
 
 showResult.addEventListener('click', listofresult);
-function listofresult(event) {
-    // let ullist = document.createElement('ul')
-    // liImge.appendChild(ullist);
-    // let listImage;
-    // for (let i = 0; i < busmall.length; i++) {
-    //     listImage = document.createElement('li');
-    //     ullist.appendChild(listImage);
-    //     listImage.textContent = `${busmall[i].name}  had ${busmall[i].counterclic} , and was seen ${busmall[i].times} times.`;
 
-    // }
-    for(let i=0;i<CAtalogbusmall.busmall.length; i++){
+
+function listofresult(event) {
+    let ullist = document.createElement('ul')
+    liImge.appendChild(ullist);
+    let listImage;
+    for (let i = 0; i < CAtalogbusmall.busmall.length; i++) {
+        listImage = document.createElement('li');
+        ullist.appendChild(listImage);
+        listImage.textContent = `${CAtalogbusmall.busmall[i].name}  had ${CAtalogbusmall.busmall[i].counterclic} , and was seen ${CAtalogbusmall.busmall[i].times} times.`;
+
+        // }
+        // for(let i=0;i<CAtalogbusmall.busmall.length; i++){
         countVote.push(CAtalogbusmall.busmall[i].counterclic);
         productShown.push(CAtalogbusmall.busmall[i].times);
 
     }
-risultChart();
+    risultChart();
 }
 
 function risultChart() {
     let ctx = document.getElementById('myChart').getContext('2d');
     let myChart = new Chart(ctx, {
-        type: 'radar',
+        type: 'bar',
         data: {
             labels: productName
             ,
@@ -154,13 +166,13 @@ function risultChart() {
                 borderColor: 'rgba(0,255,0,0.3)',
                 borderWidth: 1
             },
-        {
-            label: '# of product shown',
-            data: productShown,
-            backgroundColor:'#8A2B45',
-            borderColor:'#3A1742',
-            borderWidth:1
-        }]
+            {
+                label: '# of product shown',
+                data: productShown,
+                backgroundColor: '#8A2B45',
+                borderColor: '#3A1742',
+                borderWidth: 1
+            }]
         },
         options: {
             scales: {
@@ -174,3 +186,19 @@ function risultChart() {
 }
 
 console.log(productShown);
+//------------------------------ Add local storage-----------------------------------
+function setProduct() {
+    let productdata = JSON.stringify(CAtalogbusmall.busmall);
+    console.log(productdata);
+    localStorage.setItem('CAtalogbusmall', productdata);
+}
+
+function gitProduct() {
+    let stringdata = localStorage.getItem('CAtalogbusmall');
+    let dataofopj = JSON.parse(stringdata);
+    if (dataofopj !== null) {
+        CAtalogbusmall.busmall = dataofopj;
+    }
+
+    randomImage();
+}
